4a7d0e05d1059301b50db62443d1fb61001c54f3
Snazzie <cooper.a.m@hotmail.com>
2026-07-29 19:52:14 +0100

    style: format dynamic compare routes

--- client/src/routes/$gameid.tsx
+++ client/src/routes/$gameid.tsx
@@ -4,8 +4,8 @@ import type { GameId } from "@shared/types";
 import { useGameStore } from "../stores/game";
 
 const ROUTE_GAME_IDS: Record<string, GameId> = {
-  "fm23": "fm-2023",
-  "f125": "f1-2025",
+  fm23: "fm-2023",
+  f125: "f1-2025",
   acc: "acc",
   "ac-evo": "ac-evo",
   iracing: "iracing",
