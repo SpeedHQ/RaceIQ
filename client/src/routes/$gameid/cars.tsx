ef25d748d6bc75212400e1282cfaf1732687096e
Aaron Cooper <cooper.a.m@hotmail.com>
2026-08-06 19:23:20 +0100

    fix: remove race results card from cars page

--- client/src/routes/$gameid/cars.tsx
+++ client/src/routes/$gameid/cars.tsx
@@ -4,21 +4,13 @@ import { AcEvoCars } from "../../components/ac-evo/AcEvoCars";
 import { AccCars } from "../../components/acc/AccCars";
 import { F1Cars } from "../../components/f1/cars/F1Cars";
 import { IRacingCars } from "../../components/iracing/IRacingCars";
-import { RaceResultSummary } from "../../components/race-results/ResultSummary";
-import { gameIdForRoutePrefix } from "../../lib/game-routes";
 
 type CarsSearch = { compare?: string };
 
 function CarsRoute() {
   const { gameid } = useParams({ from: "/$gameid/cars" });
-  const gameId = gameIdForRoutePrefix(gameid);
   const page = gameid === "ac-evo" ? <AcEvoCars /> : gameid === "acc" ? <AccCars /> : gameid === "f125" ? <F1Cars /> : gameid === "iracing" ? <IRacingCars /> : <CarsPage />;
-  return (
-    <>
-      <RaceResultSummary className="mx-4 mt-4" gameId={gameId ?? null} title="Race results across all cars" />
-      {page}
-    </>
-  );
+  return page;
 }
 
 export const Route = createFileRoute("/$gameid/cars")({
