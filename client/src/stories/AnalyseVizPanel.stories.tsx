5474231c4f491ab3b895ff61969c8254de60e9e0
Snazzie <cooper.a.m@hotmail.com>
2026-09-18 11:54:51 +0100

    ci: typecheck analyse story

--- client/src/stories/AnalyseVizPanel.stories.tsx
+++ client/src/stories/AnalyseVizPanel.stories.tsx
@@ -108,8 +108,8 @@ export const ThreeDViewMenuOpen: Story = {
   render: () => <ThreeDPanelStory />,
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    await userEvent.click(canvas.getByRole("tab", { name: "3D", exact: true }));
-    await expect(canvas.getByRole("tabpanel", { name: "3D", exact: true })).toBeVisible();
+    await userEvent.click(canvas.getByRole("tab", { name: "3D" }));
+    await expect(canvas.getByRole("tabpanel", { name: "3D" })).toBeVisible();
   },
 };
 
